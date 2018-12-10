f35d9b968e8dbbd5f3b2508c322676051e5b8962
Bui Son <buison559@gmail.com>
2018-12-10 16:11:47 +0700

    Lam bai 3

--- src/jdbc/Connect.java
+++ src/jdbc/Connect.java
@@ -12,22 +12,22 @@ public class Connect {
 		try {
 			System.out.println("ket noi csdl");
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-			Connection conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;instance=BUTATU\\SQLEXPRESS;databaseName=KhachHang;","sa", "sa");
+			Connection conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;instance=BUTATU\\SQLEXPRESS;databaseName=users;","sa", "sa");
 			Statement stmt = conn.createStatement();
-			String sql21 = "insert into Table1(ID, TenKH, DiaChi, Luong) values('4', 'MaiThinh', 'Hanoi', '400.2')";
-			stmt.executeUpdate(sql21);
-			String sql22 = "update Table1 set Luong = Luong + luong * 0.1";
-			stmt.executeUpdate(sql22);
-			String sql33 = "select ID, TenKH, DiaChi, Luong from Table1";
+//			String sql21 = "insert into Table1(ID, TenKH, DiaChi, Luong) values('4', 'MaiThinh', 'Hanoi', '400.2')";
+//			stmt.executeUpdate(sql21);
+//			String sql22 = "update Table1 set Luong = Luong + luong * 0.1";
+//			stmt.executeUpdate(sql22);
+			String sql33 = "select username from acc";
 			ResultSet rs = stmt.executeQuery(sql33);
 //			stmt.executeQuery(sql33);
 			System.out.println("succes");
 			while(rs.next()) {
-				int id = rs.getInt("ID");
-				double luong = rs.getDouble("Luong");
-				String s = rs.getString("TenKH");
-				String d = rs.getString("DiaChi");
-				System.out.println("ID=" + id + "TenKH="+ s + "DiaChi=" + d + "Luong=" + luong);
+//				int id = rs.getInt("ID");
+//				double luong = rs.getDouble("Luong");
+				String s = rs.getString("username");
+//				String d = rs.getString("DiaChi");
+				System.out.println("ID=" + s);
 			}
 		}
 		catch(SQLException e) {
